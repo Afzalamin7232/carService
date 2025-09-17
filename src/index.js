7ba8988a019ced9766483e0a3d53829ebee710f4
Afzalamin7232 <afzalamin2223@gmail.com>
2025-09-17 18:40:14 +0530

    change the index fiel

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // ✅ old API (not client)
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap styles
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import AOS from "aos";
 import "aos/dist/aos.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./i18n"; 
 import { HelmetProvider } from "react-helmet-async";
 
 AOS.init({
-  duration: 1000,  // animation duration
-  once: true       // animate only once
+  duration: 1000,
+  once: true
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// ✅ Use hydrate instead of createRoot
+ReactDOM.hydrate(
   <React.StrictMode>
     <HelmetProvider>
-    < App />
+      <App />
     </HelmetProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
